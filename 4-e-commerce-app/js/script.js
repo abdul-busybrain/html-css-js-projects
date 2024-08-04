02897c3f3db16e48bc173a03415e5a6db768290e
Abdullahi(busybrain) <awwalhajine@gmail.com>
2024-08-04 22:21:04 +0100

    responsiveness, tablet, mobile

--- 4-e-commerce-app/js/script.js
+++ 4-e-commerce-app/js/script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const nav = document.getElementById("navbar");
+
+if (bar) {
+  bar.addEventListener("click", function () {
+    nav.classList.add("active");
+  });
+}
+
+if (close) {
+  close.addEventListener("click", function () {
+    nav.classList.remove("active");
+  });
+}
